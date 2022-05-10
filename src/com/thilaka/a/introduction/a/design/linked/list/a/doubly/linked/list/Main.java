f33c0f13fea41e3bb63bd44f0dd3c5e4f260ffa9
Thilaka Vijayan <v.thilaka@gmail.com>
2022-05-10 21:19:04 +1000

    Design Linked List - doubly linked list

---
+++ src/com/thilaka/a/introduction/a/design/linked/list/a/doubly/linked/list/Main.java
@@ -0,0 +1,60 @@
+package com.thilaka.a.introduction.a.design.linked.list.a.doubly.linked.list;
+
+public class Main {
+    public static void main(String[] args) {
+        MyLinkedList obj = new MyLinkedList();
+        obj.addAtHead(1);
+        obj.addAtTail(3);
+        obj.addAtIndex(1, 2);
+        obj.deleteAtIndex(1);
+        System.out.println(obj.toString());
+
+        MyLinkedList obj1 = new MyLinkedList();
+        obj1.addAtHead(1);
+        obj1.deleteAtIndex(0);
+        System.out.println(obj1.toString());
+
+        MyLinkedList obj2 = new MyLinkedList();
+        obj2.addAtHead(7);
+        obj2.addAtHead(2);
+        obj2.addAtHead(1);
+        obj2.addAtIndex(3, 0);
+        obj2.deleteAtIndex(2);
+        obj2.addAtHead(6);
+        obj2.addAtTail(4);
+        System.out.println(obj2.get(4));
+        obj2.addAtHead(4);
+        obj2.addAtIndex(5, 0);
+        obj2.addAtHead(6);
+        obj2.deleteAtIndex(1);
+        System.out.println(obj2.toString());
+
+        MyLinkedList obj3 = new MyLinkedList();
+        obj3.addAtHead(1);
+        obj3.addAtTail(3);
+        obj3.addAtIndex(1,2);
+        System.out.println(obj3.get(1));
+        obj3.deleteAtIndex(1);
+        System.out.println(obj3.get(1));
+
+       MyLinkedList obj4 = new MyLinkedList();
+       obj4.addAtHead(1);
+        obj4.addAtHead(1);
+        obj4.addAtHead(1);
+        obj4.deleteAtIndex(2);
+        obj4.deleteAtIndex(1);
+        obj4.deleteAtIndex(0);
+        System.out.println(obj4.get(0));
+
+        MyLinkedList obj5 = new MyLinkedList();
+        obj5.addAtIndex(0,10);
+        obj5.addAtIndex(0,20);
+        obj5.addAtIndex(1,30);
+        System.out.println(obj5.get(0));
+
+        MyLinkedList obj6 = new MyLinkedList();
+        obj6.addAtIndex(1,0);
+        System.out.println(obj6.get(0));
+
+    }
+}
---
+++ src/com/thilaka/a/introduction/a/design/linked/list/a/doubly/linked/list/MyLinkedList.java
@@ -0,0 +1,126 @@
+package com.thilaka.a.introduction.a.design.linked.list.a.doubly.linked.list;
+
+public class MyLinkedList {
+
+    private int val;
+    private MyLinkedList prev;
+    private MyLinkedList next;
+    private MyLinkedList head;
+
+    public MyLinkedList() {
+    }
+
+    private MyLinkedList(int val) {
+        this.val = val;
+    }
+
+    public int get(int index) {
+        MyLinkedList currNode = this.head;
+        if (currNode == null) {
+            return -1;
+        }
+        for (int i = 0; i < index; i++) {
+            if (currNode.next == null) {
+                return -1;
+            }
+            currNode = currNode.next;
+        }
+        return currNode.val;
+    }
+
+    public void addAtHead(int val) {
+        MyLinkedList newNode = new MyLinkedList(val);
+        if (this.head == null) {
+            this.head = newNode;
+            return;
+        }
+        MyLinkedList head = this.head;
+        newNode.next = head;
+        head.prev = newNode;
+        this.head = newNode;
+    }
+
+    public void addAtTail(int val) {
+        MyLinkedList newNode = new MyLinkedList(val);
+        if (this.head == null) {
+            this.head = newNode;
+            return;
+        }
+        MyLinkedList currNode = this.head;
+        while (currNode.next != null) {
+            currNode = currNode.next;
+        }
+        currNode.next = newNode;
+        newNode.prev = currNode;
+    }
+
+    public void addAtIndex(int index, int val) {
+        MyLinkedList newNode = new MyLinkedList(val);
+        if (this.head == null) {
+            if (index == 0) {
+                this.head = newNode;
+            }
+            return;
+        } else if (index == 0) {
+            addAtHead(val);
+            return;
+        }
+
+        MyLinkedList currNode = this.head;
+        for (int i = 0; i < index; i++) {
+            if (currNode.next == null) {
+                if (i == index - 1) {
+                    addAtTail(val);
+                }
+                return;
+            }
+            currNode = currNode.next;
+        }
+
+        MyLinkedList prevNode = currNode.prev;
+        currNode.prev = newNode;
+        prevNode.next = newNode;
+        newNode.prev = prevNode;
+        newNode.next = currNode;
+    }
+
+    public void deleteAtIndex(int index) {
+        MyLinkedList currNode = this.head;
+        if (currNode == null) {
+            return;
+        }
+        if (index == 0) {
+            this.head = currNode.next;
+            return;
+        }
+        for (int i = 0; i < index; i++) {
+            if (currNode.next == null) {
+                return;
+            }
+            currNode = currNode.next;
+        }
+        MyLinkedList prevNode = currNode.prev;
+        MyLinkedList nextNode = currNode.next;
+
+        prevNode.next = nextNode;
+
+        if (nextNode != null) {
+            nextNode.prev = prevNode;
+        }
+    }
+
+    @Override
+    public String toString() {
+        String outPut = String.valueOf(this.val);
+        MyLinkedList currNode = this.head;
+        if (currNode == null) {
+            return outPut;
+        }
+        outPut = String.valueOf(currNode.val);
+        while (currNode.next != null) {
+            currNode = currNode.next;
+            outPut = outPut + "->" + currNode.val;
+        }
+        return outPut;
+    }
+}
